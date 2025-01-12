38bb362897cf366c40c7521af03abebf8b0ad78d
vynhan254 <nguyenvynhan254@gmail.com>
2025-01-12 09:50:58 +0700

    push

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
     },
     server: {
         host: '0.0.0.0',
-        port: 5173,
+        port: parseInt(`${process.env.CLIENT_PORT}`) || 5173,
         proxy: {
             "/api": {
                 target: `http://127.0.0.1:${process.env.SERVER_PORT || 3000}`,
